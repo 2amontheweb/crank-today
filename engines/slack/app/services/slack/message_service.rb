9f2b8d0515d3cc7b991eee3e37d49e9c8eabd984
Jimmy Pocock <jimmypocock@yahoo.com>
2017-10-01 15:35:31 -0500

    Help message

--- engines/slack/app/services/slack/message_service.rb
+++ engines/slack/app/services/slack/message_service.rb
@@ -8,11 +8,13 @@ module Slack
       @user = user || message.user
     end
 
+    # TODO: Use translations so this doesn't get out of hand.
     def handle(task: nil)
       @task = task || find_task
       return digest if task_is 'digest'
       return today if task_is 'today'
       return reset if task_is 'reset'
+      return help if task_is 'help'
       append
     end
 
@@ -31,6 +33,15 @@ module Slack
 
     def find_task
       @task ||= text.split(' ').first
+    end
+
+    def help
+      "Commands:\n" \
+      "/crank [Type your message here.]\n" \
+      "/crank digest [List every team member's Crank message for today.]\n" \
+      "/crank reset [Reset your Crank message for today.]\n" \
+      "/crank today [Display your current Crank message for today.]\n" \
+      '/crank help [You already know what this does :) ]'
     end
 
     def reset
